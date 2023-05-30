866b47c5aa350a61a8f221459e778b5265d9ca96
mishelrodri <rp18027@ues.edu.sv>
2023-05-30 11:39:57 -0600

    Actualizando config

--- Config/Config.php
+++ Config/Config.php
@@ -1,32 +1,29 @@
-<?php 
-	
-
-	const BASE_URL = "http://localhost/ferreteriag";
-	const BASE_URL2 = "http://localhost/ferreteriag/consultas/";
-
-	//Zona horaria
-	date_default_timezone_set('America/El_Salvador');
-	
-
-	const DB_HOST = "localhost" ;
-	const DB_NAME = "db_ferreteria";
-	const DB_USER = "root";
-	const DB_PASSWORD = "";
-	const DB_CHARSET = "utf8";
+<?php
 
 
-	//Deliminadores decimal y millar Ej. 24,1989.00
-	const SPD = ".";
-	const SPM = ",";
+const BASE_URL = "https://mishelrodriguez.me/ferreteriag/";
+const BASE_URL2 = "https://mishelrodriguez.me/ferreteriag/consultas/";
 
-	//Simbolo de moneda
-	const SMONEY = "$";
-
-	//Datos envio de correo
-	const NOMBRE_REMITENTE = "Ferreteria Granadeño";
-	const EMAIL_REMITENTE = "ferreteriagranadenio12@gmail.com";
-	const NOMBRE_EMPESA = "Ferreteria Granadeño";
-	const WEB_EMPRESA = "ferreteria_granadeño.com";
+//Zona horaria
+date_default_timezone_set('America/El_Salvador');
 
 
- ?>
+const DB_HOST = "localhost";
+const DB_NAME = "db_ferreteria";
+const DB_USER = "root";
+const DB_PASSWORD = "1234";
+const DB_CHARSET = "utf8";
+
+
+//Deliminadores decimal y millar Ej. 24,1989.00
+const SPD = ".";
+const SPM = ",";
+
+//Simbolo de moneda
+const SMONEY = "$";
+
+//Datos envio de correo
+const NOMBRE_REMITENTE = "Ferreteria Granadeño";
+const EMAIL_REMITENTE = "ferreteriagranadenio12@gmail.com";
+const NOMBRE_EMPESA = "Ferreteria Granadeño";
+const WEB_EMPRESA = "ferreteria_granadeño.com";
